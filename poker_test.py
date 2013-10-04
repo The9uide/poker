176550dea7ac4cea4502a50ab37e94798db06327
The9uide <the_prince_of_nightmare@hotmail.co.jp>
2013-10-04 18:42:31 +0700

    add new 5 testcase for poker,hand_rank and straight

--- poker_test.py
+++ poker_test.py
@@ -28,8 +28,38 @@ class TestPoker(unittest.TestCase):
 		actual = poker.flush(sf)
 		expected = True
 		self.assertEqual(actual, expected)
+	
+	def test_poker_example_5(self):
+		sf = ['JC','TC','9C','8C','QC']
+		fk = ['5S','5H','5D','5C','KS']
+		actual = poker.poker([sf,fk])
+		expected = ['JC','TC','9C','8C','QC']
+		self.assertEqual(actual, expected)
 
+	def test_poker_example_6(self):
+		sf = ['JC','TC','9C','8C','7C']
+		actual = poker.hand_rank(sf)
+		expected = 8
+		self.assertEqual(actual, expected)
 
+	
+	def test_poker_example_7(self):
+		sf = ['JC','TC','9C','KC','QC']
+		actual = poker.straight(sf)
+		expected = True
+		self.assertEqual(actual, expected)
+	
+	def test_poker_example_8(self):
+		sf = ['JC','TC','AC','KC','QC']
+		actual = poker.straight(sf)
+		expected = True
+		self.assertEqual(actual, expected)
+
+	def test_poker_example_9(self):
+		sf = ['6C','TC','9C','8C','7C']
+		actual = poker.straight(sf)
+		expected = True
+		self.assertEqual(actual, expected)
 
 if __name__ == '__main__':
     unittest.main(exit=False)
