e5fd75b0ca9cbb225041bcd49801966b3614ab8e
s6070111 <s6070111@kmitl.ac.th>
2013-10-04 15:59:09 +0700

    editer poker.py add one function

--- poker.py
+++ poker.py
@@ -1,13 +1,5 @@
 def poker(hands):
-    """
-    ([hand, hand, habd, ...])->hand
-    
-    Return the best hand from list of hand
-    >>> sf = ['JC','TC','9C','8C','7C']
-    >>> fk = ['5S','5H','5D','5C','KS']
-    >>> poker([sf, fk])
-    ['JC', 'TC', '9C', '8C', '7C']
-    """
+
     return max(hands, key=hand_rank)
 
 
