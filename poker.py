791ec389c1e4f52aa9179f1e9690382ac38eb1a7
The9uide <the_prince_of_nightmare@hotmail.co.jp>
2013-10-05 23:20:35 +0700

    add proviso in function hand_rank and add function two_pair

--- poker.py
+++ poker.py
@@ -12,6 +12,20 @@ def hand_rank(hand):
 	"""
 	if straight_flush(hand):
 		return 8
+	elif four_of_a_kind(hand):
+		return 7
+	elif full_house(hand):
+		return 6
+	elif flush(hand):
+		return 5
+	elif straight(hand):
+		return 4
+	elif three_of_a_kind(hand):
+		return 3
+	elif two_pair(hand) :
+		return 3
+	elif one_pair(hand):
+		return 2
 	else:
 		return 0
 
@@ -34,7 +48,7 @@ def straight(hand):
 	suit = "".join(suits)
 	if suit in "2345A123456789T789JT89JQT9JKQTAJKQT":
 		return True
-	print suit,"+++++"
+
 	return False
 
 def flush(hand):
@@ -46,16 +60,67 @@ def flush(hand):
 	if suits.count(suits[0]) == 5:
 		return True
 	return False
-def four_of_a_kind(hand):
-        """
-                (hand) -> Bool
-        Return True if hand is four_of_a_kind,
-        Flase otherwise
-        """
-        suits = [r for r,s in hand]
-        for card in suits:
-                if suits.count(card) == 4:
-                        return True
-        return False
 
+def four_of_a_kind(hand):
+	"""
+		(hand) -> Bool
+	Return True if hand is four_of_a_kind,Flase otherwise
+	"""
+	suits = [r for r,s in hand]
+	for card in suits:
+		if suits.count(card) == 4:
+			return True
+	return False
+
+def full_house(hand):
+	"""
+		(hand) -> Bool
+	Return True if hand is full_house,Flase otherwise
+	"""
+	if three_of_a_kind(hand) and one_pair(hand):
+		return True
+	return False
+
+def three_of_a_kind(hand):
+	"""
+		(hand) -> Bool
+	Return True if hand is three_of_a_kind,Flase otherwise
+	"""
+	suits = [r for r,s in hand]
+	for card in suits:
+		if suits.count(card) == 3:
+			return True
+	return False
+
+def one_pair(hand):
+	"""
+		(hand) -> Bool
+	Return True if hand is one_pair,Flase otherwise
+	"""
+	suits = [r for r,s in hand]
+	pair = {}
+	for card in suits:
+		if card in pair:
+			pair[card] += 1
+		else:
+			pair[card] = 1
+	if pair.values().count(2) == 1:
+		return True
+	return False
+
+def two_pair(hand):
+	"""
+		(hand) -> Bool
+	Return True if hand is two_pair,Flase otherwise
+	"""
+	suits = [r for r,s in hand]
+	pair = {}
+	for card in suits:
+		if card in pair:
+			pair[card] += 1
+		else:
+			pair[card] = 1
+	if pair.values().count(2) >= 2:
+		return True
+	return False
 
