c27eace6f01301d5b6b1bfd08f69198e66211ce0
The9uide <the_prince_of_nightmare@hotmail.co.jp>
2013-10-10 15:45:47 +0700

    add proviso in function hand_rank add 4 new function

--- poker.py
+++ poker.py
@@ -15,19 +15,26 @@ def hand_rank(hand):
 	elif straight_flush(hand):
 		return 8 + max(compare_card(hand))
 	elif four_of_a_kind(hand):
-		return 7 + max(compare_card(hand))
+		max_four = max_four_of_a_kind(hand)
+		return 7 + max(compare_card(max_four))
 	elif full_house(hand):
-		return 6 + max(compare_card(hand))
+		max_full = max_three_of_a_kind(hand)
+		return 6 + max(compare_card(max_full))
 	elif flush(hand):
 		return 5 + max(compare_card(hand))
 	elif straight(hand):
 		return 4 + max(compare_card(hand))
 	elif three_of_a_kind(hand):
-		return 3 + max(compare_card(hand))
+		max_three = max_three_of_a_kind(hand)
+		return 3 + max(compare_card(max_three))
 	elif two_pair(hand):
-		return 3 + max(compare_card(hand))
+		max_two = max_two_pair(hand)
+		max_two_single = max_two_pair(hand,True)
+		return 3 + max(compare_card(max_two)) +  max(compare_card(max_two_single))/100.0
 	elif one_pair(hand):
-		return 2 + max(compare_card(hand))
+		max_one = max_two_pair(hand)
+		max_one_single = max_two_pair(hand,True)
+		return 2 +  max(compare_card(max_one)) +  max(compare_card(max_one_single))/100.0
 	else:
 		return 1 + max(compare_card(hand))
 
@@ -43,7 +50,7 @@ def straight(hand):
 		(hand) -> Bool
 	Return True if hand is straight,Flase otherwise
 	"""
-	suits = [r for r,s in hand]
+	suits = creat_suits(hand)
 	suits.sort()
 	suit = "".join(suits)
 	return suit in "2345A123456789T789JT89JQT9JKQTAJKQT"
@@ -61,7 +68,7 @@ def four_of_a_kind(hand):
 		(hand) -> Bool
 	Return True if hand is four_of_a_kind,Flase otherwise
 	"""
-	suits = [r for r,s in hand]
+	suits = creat_suits(hand)
 	return suits.count(suits[0]) == 4 or suits.count(suits[1]) == 4 
 
 def full_house(hand):
@@ -76,7 +83,7 @@ def three_of_a_kind(hand):
 	(hand) -> Bool
 	Return True if hand is three_of_a_kind,Flase otherwise
 	"""
-	suits = [r for r,s in hand]
+	suits = creat_suits(hand)
 	return suits.count(suits[0]) == 3 or suits.count(suits[1]) == 3 or suits.count(suits[2]) == 3
 
 def one_pair(hand):
@@ -84,10 +91,11 @@ def one_pair(hand):
 		(hand) -> Bool
 	Return True if hand is one_pair,Flase otherwise
 	"""
-	suits = [r for r,s in hand]
+	suits = creat_suits(hand)
 	pair = {}
 	for card in suits:
-		if card in pair:pair[card] += 1
+		if card in pair:
+			pair[card] += 1
 		else:
 			pair[card] = 1
 	return pair.values().count(2) == 1
@@ -97,7 +105,7 @@ def two_pair(hand):
 		(hand) -> Bool
 	Return True if hand is two_pair,Flase otherwise
 	"""
-	suits = [r for r,s in hand]
+	suits = creat_suits(hand)
 	pair = {}
 	for card in suits:
 		if card in pair :pair[card] += 1
@@ -152,3 +160,42 @@ def compare_card(hand):
 	for card in hand:
 		quality.append((gen_card().index(card)/4)/100.0)
 	return quality
+
+def creat_suits(hand):
+	return [r for r,s in hand]
+
+def max_four_of_a_kind(hand):
+	suits = creat_suits(hand)
+	if suits.count(suits[0]) == 4 : return [suits[0]+"H"]
+	elif suits.count(suits[1]) == 4 : return [suits[1]+"H"]
+
+def max_three_of_a_kind(hand):
+	suits = creat_suits(hand)
+	if suits.count(suits[0]) == 3 : return [suits[0]+"H"]
+	elif suits.count(suits[1]) == 3 : return [suits[1]+"H"]
+	elif suits.count(suits[2]) == 3 : return [suits[2]+"H"]
+
+def max_two_pair(hand,single = False):
+	suits = creat_suits(hand)
+	pair = {}
+	max_hand = []
+	for card in suits:
+		if card in pair :
+			pair[card] += 1
+			if pair[card] == 2:
+				max_hand.append(card)
+		else:
+			pair[card] = 1
+	if single:
+		max_hand =  list(set(pair.keys())-set(max_hand))
+	return [r+"H" for r in max_hand]
+
+def start_poker():
+	player = []
+	for i in xrange(input("How many player? : ")):
+		player.append(input("Player"+ str(i+1)+" : "))
+	player_winner = multi_winner(player)
+	for i in player_winner:
+		print str(i)
+
+start_poker()
